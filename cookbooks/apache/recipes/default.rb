a6bde122d9110a3665db5127f27e3f80b2ef4ae1
ramvilas619 <ramvilas619@gmail.com>
2017-11-21 05:40:29 +0000

    Addaded basic apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+#Install httpd package
+package 'apache' do
+	package_name 'httpd'
+	action :install
+end
+
+service 'apache' do
+	service_name 'httpd'
+	action [:start, :enable]
+end
